7d00e34a5ab4d71b696d9225bb1e9c9e6152c48e
Victor Shepardson <victor.shepardson@gmail.com>
2022-02-17 13:34:46 +0000

    notebook

--- src/example-app/example-app.cpp
+++ src/example-app/example-app.cpp
@@ -1,9 +1,13 @@
 #include <torch/script.h> // One-stop header.
+// #include <torch/all.h>
+// #include <soundio/soundio.h>
 
 #include <iostream>
 #include <memory>
 
-// sugar for the appalling tuple access syntax...
+//https://github.com/pytorch/pytorch/blob/master/torch/csrc/jit/OVERVIEW.md#executing-programs
+
+// sugar for the tuple access syntax
 torch::jit::IValue getitem(torch::jit::IValue v, int i) {
   return v.toTuple()->elements()[i];
 }
@@ -29,20 +33,45 @@ int main(int argc, const char* argv[]) {
   std::cout << "ok\n";
 
   // Create a vector of inputs.
-  torch::jit::IValue h, x, u;
+  torch::jit::IValue h, x;
   h = torch::zeros({1, 128}); // TODO: get h0 out of model
   x = torch::zeros({1, 1});
 
-  for (int i=0; i<64; i++){
-    u = torch::rand({1, 1});
-    std::vector<torch::jit::IValue> inputs = {h, x, u};
+  auto inputs = std::vector<torch::jit::IValue>(3);
 
-    // Execute the model and turn its output into a tensor.
-    auto outputs = module.forward(inputs);
-    h = getitem(outputs, 0);//.toTensor();
-    x = getitem(outputs, 1);//.toTensor();
+  // warm-up for timing
+  inputs[0] = h;
+  inputs[1] = x;
+  inputs[2] = torch::rand({1, 1});
+  auto outputs = module.forward(inputs);
+  // auto staticmodule = StaticModule(module);
+  // auto outputs = staticmodule(inputs);
 
-    std::cout << x.toTensor().item<float>() << '\n';
+  float wave[8000];
+  const int block_size = 64;
+
+  for (int block=0; block<4096; block++){
+    auto start = std::chrono::steady_clock::now();
+    for (int i=0; i<block_size; i++){
+      // std::vector<torch::jit::IValue> inputs = {h, x, u};
+      
+      inputs[0] = h;
+      inputs[1] = x;
+      inputs[2] = torch::rand({1, 1});
+
+      outputs = module.forward(inputs);
+      // auto outputs = staticmodule(inputs);
+
+      h = getitem(outputs, 0);//.toTensor();
+      x = getitem(outputs, 1);//.toTensor();
+
+      wave[i] = x.toTensor().item<float>();
+
+      // std::cout << x.toTensor().item<float>() << '\n';
+    }
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cout << block_size/elapsed_seconds.count() << '\n';
   }
 
 }
